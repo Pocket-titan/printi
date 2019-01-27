1c6d448b3faea5f51176e22efc4c00c2ffc6b06c
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2019-01-27 21:19:10 +0100

    uhhhh

---
+++ docs/precache-manifest.f2b3d392d54c96fc7914260f058e4309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "428bf0190a5a54401281",
+    "url": "/static/css/main.7838fd43.chunk.css"
+  },
+  {
+    "revision": "428bf0190a5a54401281",
+    "url": "/static/js/main.428bf019.chunk.js"
+  },
+  {
+    "revision": "4c27edead7f3fb300621",
+    "url": "/static/js/1.4c27edea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c0d89d71ef12d989fb6e37ad85e2367",
+    "url": "/static/media/printi_logo_transparent.8c0d89d7.svg"
+  },
+  {
+    "revision": "efa8a334f3a1b43d1b398a5e12d01514",
+    "url": "/index.html"
+  }
+];
